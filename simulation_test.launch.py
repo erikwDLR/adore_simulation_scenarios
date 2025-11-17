23b87825f3da85461d7d7a0501d2f320d3685c07
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-11-17 16:47:14 +0100

    update licence headers

--- simulation_test.launch.py
+++ simulation_test.launch.py
@@ -1,3 +1,16 @@
+# ********************************************************************************
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+# ********************************************************************************
+
 
 from launch import LaunchDescription
 import os
@@ -22,13 +35,6 @@ def generate_launch_description():
     vehicle_model_file = vehicle_param + "/NGC.json"
 
     return LaunchDescription([
-        *create_visualization_nodes(
-            whitelist=["ego_vehicle"],
-            asset_folder=map_image_folder,
-            visualization_offset=(606453.910, 5797315.369),
-
-        ),
-
         *create_simulated_vehicle_nodes(
             namespace="ego_vehicle",
             start_pose=(606500.065, 5797318.038, 3.04),
